12fb4b3d740a7081bfb0081a113f617349ef8051
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-30 19:20:40 +0200

    EveryIndividualBookCostsEightEuros

--- Kata.HarryPotter/WhenCalculatingBookPrices.cs
+++ Kata.HarryPotter/WhenCalculatingBookPrices.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace Kata.HarryPotter
 {
@@ -6,9 +7,23 @@ namespace Kata.HarryPotter
     public class WhenCalculatingBookPrices
     {
         [Test]
-        public void T()
+        public void EveryIndividualBookCostsEightEuros()
         {
-
+            var books = new SomeBookRepository().GetAllBooks();
+            Assert.That(books.Select(book => book.Price), Is.All.EqualTo(8m));
         }
+    }
+
+    public class SomeBookRepository
+    {
+        public Book[] GetAllBooks()
+        {
+            return new Book[] { new Book(), new Book(), new Book(), new Book(), new Book() };
+        }
+    }
+
+    public class Book
+    {
+        public decimal Price { get; } = 8;
     }
 }
